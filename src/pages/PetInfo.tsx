5edbb3de428a8b9cc4415571235df4a8b1dfdef5
Walber Fellipe <walberfellipe18@gmail.com>
2021-04-06 23:45:21 -0300

    Up to date

--- src/pages/PetInfo.tsx
+++ src/pages/PetInfo.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, StyleSheet, TextInput, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Switch,
+} from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -9,10 +17,25 @@ import {
 export default function PetInfo() {
   const navigation = useNavigation();
 
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
   return (
     <View style={styles.petInfoContainer}>
-      <Image source={require("../images/image.jpg")} style={styles.petImage} />
-      <Text style={styles.petInfoName}> Bob </Text>
+      <TouchableOpacity activeOpacity={0.5} style={styles.petInfoButton}>
+        <Image
+          source={require("../images/image.jpg")}
+          style={styles.petInfoImage}
+        />
+        <Text style={styles.petInfoText}>Bob</Text>
+      </TouchableOpacity>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 }
@@ -28,15 +51,27 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
   },
-
-  petImage: {
+  petInfoButton: {
+    backgroundColor: "#F5F5F5",
+    borderWidth: 1,
+    borderColor: "#1670AE",
+    color: "#1670AE",
+    borderRadius: 75,
+    alignItems: "center",
     marginTop: "5%",
-    width: "30%",
+    width: "35%",
     height: "20%",
-    borderRadius: 20,
+    marginLeft: "10%",
+    marginRight: "10%",
+    flexDirection: "column",
+  },
+  petInfoImage: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 75,
   },
 
-  petInfoName: {
+  petInfoText: {
     fontFamily: "Nunito_800ExtraBold",
     fontSize: 33,
   },
